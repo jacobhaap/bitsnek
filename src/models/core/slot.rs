df2ea29f3d77b4c9b55b747589f2d6707b276631
Jacob Haap <33384469+jacobhaap@users.noreply.github.com>
2021-01-03 01:54:39 -0500

    Update slot.rs

--- src/models/core/slot.rs
+++ src/models/core/slot.rs
@@ -2,7 +2,7 @@
 pub enum Slot {
     Air,
     Wall,
-    Snake,
+    Snek,
     Apple
 }
 
@@ -10,7 +10,7 @@ pub fn u8_to_slot(n: u8) -> Slot {
     match n {
         0 => Slot::Air,
         1 => Slot::Wall,
-        2 => Slot::Snake,
+        2 => Slot::Snek,
         3 => Slot::Apple,
         _ => panic!(format!("Invalid integer to map to Slot (received {})", n))
     }
@@ -20,7 +20,7 @@ pub fn slot_to_u8(slot: Slot) -> u8 {
     match slot {
         Slot::Air => 0,
         Slot::Wall => 1,
-        Slot::Snake => 2,
+        Slot::Snek => 2,
         Slot::Apple => 3
     }
 }
